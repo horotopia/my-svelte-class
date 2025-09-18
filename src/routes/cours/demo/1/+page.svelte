cee07fe5ee05103695bc9419e57f1af9ee8eee58
Guirado Léo <horotopia@hotmail.com>
2025-09-18 22:57:31 +0200

    fix: Correct spelling of 'Connection' to 'Connexion' in demo 1 button

--- src/routes/cours/demo/1/+page.svelte
+++ src/routes/cours/demo/1/+page.svelte
@@ -5,5 +5,5 @@
 <button 
 	class="border p-2 rounded-xl cursor-pointer"
 	on:click={() => isConnected = !isConnected}
->Connection</button>
+>Connexion</button>
 <p>{isConnected ? 'Vous êtes connecté' : 'Vous n\'êtes pas connecté' }</p>
